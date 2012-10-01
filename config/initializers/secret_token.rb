26d1e8d76218811a152a6688af1240b009322864
aakintil <aakintil@andrew.cmu.edu>
2012-10-01 16:50:00 -0400

    began project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BarcodeScanner::Application.config.secret_token = '415cec9668cf0f4708d04a241713ac734d6b7bef5762cde2b33cd5c7f0ef3b7103c891690bc1ea49e48913cbb5e15dcba364dccd1fce083b852be9538347d96d'
